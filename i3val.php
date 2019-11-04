2e45c10eee9e69be07aa0c362b1b9ac393219036
B. Endres <endres@systopia.de>
2019-11-04 11:27:47 +0100

    [#20] make sure form processor integration is only called when module istalled

--- i3val.php
+++ i3val.php
@@ -25,7 +25,9 @@ use \Symfony\Component\DependencyInjection\ContainerBuilder;
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_container/
  */
 function i3val_civicrm_container(ContainerBuilder $container) {
-  $container->addCompilerPass(new Civi\I3val\ActionProvider\Action\RequestContactUpdate());
+  if (class_exists("Civi\ActionProvider\Action\AbstractAction")) {
+    $container->addCompilerPass(new Civi\I3val\ActionProvider\Action\RequestContactUpdate());
+  }
 }
 
 /**
